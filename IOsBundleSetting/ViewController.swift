cff67e3479812c6fc72de93f36968b1e29f170bd
rogerzuchu21022019 <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-05-25 10:29:02 +0700

    Initial Commit

--- IOsBundleSetting/ViewController.swift
+++ IOsBundleSetting/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UserDefaults.standard.register(defaults: [String : Any]())
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        let userDefault:UserDefaults = UserDefaults.standard
+        var fullname = userDefault.string(forKey: "fullname")
+        let email = userDefault.string(forKey: "email")
+        let phone = userDefault.string(forKey: "phone")
+        let address = userDefault.string(forKey: "address")
+        let isUserEnabled = userDefault.bool(forKey: "notification")
+        
+        
+         userDefault.setValue("NamZuChu", forKey: "fullname")
+        userDefault.setValue("vu.nam@hitachivantara.com", forKey: "email")
+        userDefault.synchronize()
+       
+        print("\(fullname!)")
+        print("\(email!)")
+        print("\(phone!)")
+        print("\(address!)")
+        print("\(isUserEnabled)")
+    }
 
 }
 
